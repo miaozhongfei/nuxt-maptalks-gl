e5b3db1359323b979d36ced502e980b642c8a56b
miaozf <1103520414@qq.com>
2026-07-30 20:27:23 +0800

    test: 8.11 ComponentImpl 改回模板内联 :options 验证 bindOptionsRebuild fix

--- examples/demo-suite/app/components/example2/interaction/edit-textbox/ComponentImpl.vue
+++ examples/demo-suite/app/components/example2/interaction/edit-textbox/ComponentImpl.vue
@@ -14,7 +14,17 @@
           :coordinates="[121.5057, 31.2453]"
           :width="200"
           :height="90"
-          :options="tbOpts"
+          :options="{
+            draggable: true,
+            textStyle: {
+              wrap: true,
+              padding: [12, 8],
+              verticalAlignment: 'top',
+              horizontalAlignment: 'right',
+              symbol: { textFaceName: 'monospace', textFill: '#34495e', textHaloFill: '#fff', textHaloRadius: 4, textSize: 18, textWeight: 'bold' },
+            },
+            boxSymbol: { markerType: 'square', markerFill: 'rgb(135,196,240)', markerFillOpacity: 0.9, markerLineColor: '#34495e', markerLineWidth: 1 },
+          }"
         />
       </MaptalksVectorLayer>
     </MaptalksMap>
@@ -27,18 +37,6 @@
 
 <script setup lang="ts">
 const tRef = ref<MaptalksTextBoxExposed | null>(null)
-
-const tbOpts = {
-  draggable: true,
-  textStyle: {
-    wrap: true,
-    padding: [12, 8],
-    verticalAlignment: 'top',
-    horizontalAlignment: 'right',
-    symbol: { textFaceName: 'monospace', textFill: '#34495e', textHaloFill: '#fff', textHaloRadius: 4, textSize: 18, textWeight: 'bold' },
-  },
-  boxSymbol: { markerType: 'square', markerFill: 'rgb(135,196,240)', markerFillOpacity: 0.9, markerLineColor: '#34495e', markerLineWidth: 1 },
-}
 
 function startEdit() { tRef.value?.geometry?.startEdit?.() }
 function endEdit() { tRef.value?.geometry?.endEdit?.() }
